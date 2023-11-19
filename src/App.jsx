9406a6084ffa8a9c9c4998c382be1b12e9a6c2db
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2023-11-19 22:51:32 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,331 @@
+import { v4 as uuidv4 } from 'uuid';
+import Work from './Work';
+import { useState } from 'react';
+import './_App.scss';
+
+function App() {
+  const works = [
+    {
+      key: uuidv4(),
+      id: '202021411',
+      title: 'Lotto720+',
+      designer: '전승리',
+      thumbnail: './assets/thumbs/smucd_202021411.jpg',
+      link: 'https://smucd2023.com/project/lotto720/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '202021403',
+      title: '젊음의 늙음',
+      designer: '김란수',
+      thumbnail: './assets/thumbs/smucd_202021403.jpg',
+      link: 'https://smucd2023.com/project/%ec%a0%8a%ec%9d%8c%ec%9d%98-%eb%8a%99%ec%9d%8c/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '202020383',
+      title: '남성 맞춤 케어 서비스 MMA',
+      designer: '탁경민',
+      thumbnail: './assets/thumbs/smucd_202020383.jpg',
+      link: 'https://smucd2023.com/project/mma/',
+      isSelected: false,
+    },
+    {
+      key: uuidv4(),
+      id: '202020293',
+      title: 'Fitness toasties : 게임으로 즐기는 홈 트레이닝!',
+      designer: '유지연',
+      thumbnail: './assets/thumbs/smucd_202020293.jpg',
+      link: 'https://smucd2023.com/project/fitness-toasties-%ea%b2%8c%ec%9e%84%ec%9c%bc%eb%a1%9c-%ec%a6%90%ea%b8%b0%eb%8a%94-%ed%99%88-%ed%8a%b8%eb%a0%88%ec%9d%b4%eb%8b%9d/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '202020289',
+      title: 'VISVO',
+      designer: '오하진',
+      thumbnail: './assets/thumbs/smucd_202020289.jpg',
+      link: 'https://smucd2023.com/project/visualize-sound-wave-visvo/',
+      isSelected: false,
+    },
+    {
+      key: uuidv4(),
+      id: '201921402',
+      title: 'TEAMPL',
+      designer: '김효정',
+      thumbnail: './assets/thumbs/smucd_201921402.jpg',
+      link: 'https://smucd2023.com/project/teampl/',
+      isSelected: false,
+    },
+    {
+      key: uuidv4(),
+      id: '201920753',
+      title: '듣고싶은 잔소리, VOICER',
+      designer: '남우빈',
+      thumbnail: './assets/thumbs/smucd_201920753.jpg',
+      link: 'https://smucd2023.com/project/%eb%93%a3%ea%b3%a0%ec%8b%b6%ec%9d%80-%ec%9e%94%ec%86%8c%eb%a6%ac-voicer/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920319',
+      title: '내일일기',
+      designer: '구유정, 박채원',
+      thumbnail: './assets/thumbs/smucd_201920319,201920333.jpg',
+      link: 'https://smucd2023.com/project/%eb%82%b4%ec%9d%bc%ec%9d%bc%ea%b8%b0/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920314',
+      title: '당신의 깔끔한 이별을 위한 KIT : LOVEND',
+      designer: '홍수아',
+      thumbnail: './assets/thumbs/smucd_201920314.jpg',
+      link: 'https://smucd2023.com/project/%eb%8b%b9%ec%8b%a0%ec%9d%98-%ea%b9%94%eb%81%94%ed%95%9c-%ec%9d%b4%eb%b3%84%ec%9d%84-%ec%9c%84%ed%95%9c-kit-lovend/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920313',
+      title: '사용자 맞춤형 공유오피스 예약 어플: 스픽',
+      designer: '홍세령',
+      thumbnail: './assets/thumbs/smucd_201920313.jpg',
+      link: 'https://smucd2023.com/project/%ec%82%ac%ec%9a%a9%ec%9e%90-%eb%a7%9e%ec%b6%a4%ed%98%95-%ea%b3%b5%ec%9c%a0%ec%98%a4%ed%94%bc%ec%8a%a4-%ec%98%88%ec%95%bd-%ec%96%b4%ed%94%8c-%ec%8a%a4%ed%94%bd/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920299',
+      title: '1OOOO ARCHIVE',
+      designer: '조현서',
+      thumbnail: './assets/thumbs/smucd_201920299.jpg',
+      link: 'https://smucd2023.com/project/1oooo-archive/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920297',
+      title: '오늘의 빙고',
+      designer: '조소현',
+      thumbnail: './assets/thumbs/smucd_201920297.png',
+      link: 'https://smucd2023.com/project/%ec%98%a4%eb%8a%98%ec%9d%98-%eb%b9%99%ea%b3%a0/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920284',
+      title: 'momenti',
+      designer: '장수현',
+      thumbnail: './assets/thumbs/smucd_201920284.jpg',
+      link: 'https://smucd2023.com/project/momenti/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920272',
+      title: '손쉬운 정보 저장의 시작, Clipin',
+      designer: '이현아',
+      thumbnail: './assets/thumbs/smucd_201920272.jpg',
+      link: 'https://smucd2023.com/project/%ec%86%90%ec%89%ac%ec%9a%b4-%ec%a0%95%eb%b3%b4-%ec%a0%80%ec%9e%a5%ec%9d%98-%ec%8b%9c%ec%9e%91-clipin/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920269',
+      title: 'NOW-SEOUL : 한국 신진 작가전',
+      designer: '이채은, 임채원',
+      thumbnail: './assets/thumbs/smucd_201920269,201920279.jpg',
+      link: 'https://smucd2023.com/project/gallery-eoma/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920263',
+      title: 'DOOMOPOLY',
+      designer: '이예진',
+      thumbnail: './assets/thumbs/smucd_201920263.jpg',
+      link: 'https://smucd2023.com/project/doomopoly/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920245',
+      title: '퓨전 괴식 연구가',
+      designer: '신예린',
+      thumbnail: './assets/thumbs/smucd_201920245.jpg',
+      link: 'https://smucd2023.com/project/%ed%93%a8%ec%a0%84-%ea%b4%b4%ec%8b%9d-%ec%97%b0%ea%b5%ac%ea%b0%80/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920242',
+      title: '아마도 이상적인 선택들',
+      designer: '손수민',
+      thumbnail: './assets/thumbs/smucd_201920242.jpg',
+      link: 'https://smucd2023.com/project/%ec%95%84%eb%a7%88%eb%8f%84-%ec%9d%b4%ec%83%81%ec%a0%81%ec%9d%b8-%ec%84%a0%ed%83%9d%eb%93%a4/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920240',
+      title: 'Lawe',
+      designer: '배일영',
+      thumbnail: './assets/thumbs/smucd_201920240.jpg',
+      link: 'https://smucd2023.com/project/life-anywhere-work-everywhere/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920231',
+      title: "EVE's WAR : 인류멸망 시나리오",
+      designer: '민서영',
+      thumbnail: './assets/thumbs/smucd_201920231.jpg',
+      link: 'https://smucd2023.com/smucd201920231/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920219',
+      title: 'LINKCON',
+      designer: '김현경',
+      thumbnail: './assets/thumbs/smucd_201920219.jpg',
+      link: 'https://smucd2023.com/project/linkcon/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920216',
+      title: '소통하며 읽는 즐거운 독서, 북적북적',
+      designer: '김하영',
+      thumbnail: './assets/thumbs/smucd_201920216.jpg',
+      link: 'https://smucd2023.com/project/%ec%86%8c%ed%86%b5%ed%95%98%eb%a9%b0-%ec%9d%bd%eb%8a%94-%ec%a6%90%ea%b1%b0%ec%9a%b4-%eb%8f%85%ec%84%9c-%eb%b6%81%ec%a0%81%eb%b6%81%ec%a0%81/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920211',
+      title: '해파리교를 믿으시나요?',
+      designer: '김지향',
+      thumbnail: './assets/thumbs/smucd_201920211.jpg',
+      link: 'https://smucd2023.com/project/%ed%95%b4%ed%8c%8c%eb%a6%ac%ea%b5%90%eb%a5%bc-%eb%af%bf%ec%9c%bc%eb%a9%b4-%ec%98%81%ec%83%9d%ec%9d%84-%eb%88%84%eb%a6%b4-%ec%88%98-%ec%9e%88%ec%8a%b5%eb%8b%88%eb%8b%a4/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920207',
+      title: '전시사진 정리부터 소통까지 A-mu',
+      designer: '김예은',
+      thumbnail: './assets/thumbs/smucd_201920207.jpg',
+      link: 'https://smucd2023.com/project/%ec%a0%84%ec%8b%9c%ec%82%ac%ec%a7%84-%ec%a0%95%eb%a6%ac%eb%b6%80%ed%84%b0-%ec%86%8c%ed%86%b5%ea%b9%8c%ec%a7%80-a-mu/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920194',
+      title: 'Number',
+      designer: '권소연',
+      thumbnail: './assets/thumbs/smucd_201920194.jpg',
+      link: 'https://smucd2023.com/project/number/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920191',
+      title: '운명을 찾아가는 길 나만의 부적을 만나다 : 운세부적',
+      designer: '곽인아',
+      thumbnail: './assets/thumbs/smucd_201920191.jpg',
+      link: 'https://smucd2023.com/project/%ec%9a%b4%eb%aa%85%ec%9d%84-%ec%b0%be%ec%95%84%ea%b0%80%eb%8a%94-%ea%b8%b8%eb%82%98%eb%a7%8c%ec%9d%98-%eb%b6%80%ec%a0%81%ec%9d%84-%eb%a7%8c%eb%82%98%eb%8b%a4-%ec%9a%b4%ec%84%b8%eb%b6%80%ec%a0%81/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201920189',
+      title: '노틸러스 프로젝트',
+      designer: '강현구',
+      thumbnail: './assets/thumbs/smucd_201920189.jpg',
+      link: 'https://smucd2023.com/project/%eb%85%b8%ed%8b%b8%eb%9f%ac%ec%8a%a4-%ed%94%84%eb%a1%9c%ec%a0%9d%ed%8a%b8/',
+      isSelected: false,
+    },
+    {
+      key: uuidv4(),
+      id: '201820279',
+      title: '나 혼자만 지적재산권 마음대로 사용하기',
+      designer: '이주헌',
+      thumbnail: './assets/thumbs/smucd_201820279.jpg',
+      link: 'https://smucd2023.com/project/%eb%82%98-%ed%98%bc%ec%9e%90%eb%a7%8c-%ec%a7%80%ec%a0%81%ec%9e%ac%ec%82%b0%ea%b6%8c-%eb%a7%88%ec%9d%8c%eb%8c%80%eb%a1%9c-%ec%82%ac%ec%9a%a9%ed%95%98%ea%b8%b0/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201820261',
+      title: 'GIFT FUNDING RIBBON',
+      designer: '유이현',
+      thumbnail: './assets/thumbs/smucd_201820261.jpg',
+      link: 'https://smucd2023.com/project/gift-funding-ribbon/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201720255',
+      title: 'Red vs Blue',
+      designer: '안채영',
+      thumbnail: './assets/thumbs/smucd_201720255.jpg',
+      link: 'https://smucd2023.com/project/red-vs-blue/',
+      isSelected: true,
+    },
+    {
+      key: uuidv4(),
+      id: '201720225',
+      title: '운명 개조 서비스',
+      designer: '김휘록',
+      thumbnail: './assets/thumbs/smucd_201720225.jpg',
+      link: 'https://smucd2023.com/project/%ec%9a%b4%eb%aa%85-%ea%b0%9c%ec%a1%b0-%ec%84%9c%eb%b9%84%ec%8a%a4/',
+      isSelected: true,
+    },
+  ];
+
+  const selectedWorks = works.filter((eachWork) => {
+    return eachWork.isSelected === true;
+  });
+
+  const getSixWork = () => {
+    const six = [];
+    for (let cnt = 0; cnt < 6; cnt++) {
+      const idx = Math.floor(Math.random() * selectedWorks.length);
+      six.push(selectedWorks[idx]);
+      selectedWorks.splice(idx, 1);
+    }
+    return six;
+  };
+
+  const [selectedSix, setSelectedSix] = useState(getSixWork());
+
+  const handleClick = () => {
+    setSelectedSix(getSixWork());
+  };
+
+  return (
+    <div className={`container`}>
+      <h1 className={`title`}>Selected Works of IDP23</h1>
+      <div className={`grid`}>
+        {selectedSix.map((eachWork) => {
+          return (
+            <Work
+              key={eachWork.key}
+              title={eachWork.title}
+              designer={eachWork.designer}
+              thumbnail={eachWork.thumbnail}
+              link={eachWork.link}></Work>
+          );
+        })}
+      </div>
+      <div className={`button`} onClick={handleClick}>
+        Show Others
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Work.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import Classes from './_Work.module.scss';
+
+const Work = ({ title, designer, thumbnail, link }) => {
+  const handleClick = () => {
+    window.location.href = link;
+  };
+
+  return (
+    <div className={`${Classes.work}`} onClick={handleClick}>
+      <img className={`${Classes.thumbnail}`} src={`${thumbnail}`}></img>
+      <div className={`${Classes.text}`}>
+        <div className={`${Classes.padding}`}></div>
+        <h2 className={`${Classes.title}`}>{title}</h2>
+        <h3 className={`${Classes.designer}`}>{designer}</h3>
+      </div>
+    </div>
+  );
+};
+
+Work.propTypes = {
+  title: PropTypes.string,
+  designer: PropTypes.string,
+  thumbnail: PropTypes.string,
+  link: PropTypes.string,
+};
+
+export default Work;
